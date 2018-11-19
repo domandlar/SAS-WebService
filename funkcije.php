2d8c59949075b86025ce72137286521bc5f9cd7a
Domagoj <domagoj.andlar@gmail.com>
2018-11-19 17:58:29 +0100

    Kreirana funkcija za kreiranje JSON odgovora.

--- funkcije.php
+++ funkcije.php
@@ -17,3 +17,12 @@ function DohvatiIzBaze($upit)
     $db->zatvoriDB();
     return $rez;
 }
+function DeliverResponse($status, $message, $data)
+{
+    $response['status'] = $status;
+    $response['message'] = $message;
+
+    $response['data'] = json_encode($data);
+    $json_response = json_encode($response);
+    echo $json_response;
+}
